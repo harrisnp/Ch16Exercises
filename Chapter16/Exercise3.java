603d92ad373a7a147366df1a6e283dc43d62a8b2
Nathan Harris <harrisnp@s.dcsdk12.org>
2019-09-06 13:43:02 -0600

    Redid Commit

---
+++ Chapter16/Exercise3.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Exercise3
+{
+    public static void main(String[]args)
+    {
+        Scanner scan = new Scanner (System.in);
+        double capacity;
+        double gage;
+        double mpg;
+        double miLeft;
+
+        System.out.println("Tank capacity: ");
+        capacity = scan.nextDouble();
+        System.out.println("Gage reading: (percentage) ");
+        gage = scan.nextDouble(); 
+        System.out.println("MPG: ");
+        mpg = scan.nextDouble();
+
+        miLeft = (capacity * (gage/100) * mpg);
+
+        if (miLeft >= 200){
+            System.out.println("Safe to Proceed.");
+        } else {
+            System.out.println("Get Gas!");
+        }
+
+        
+    }
+}
